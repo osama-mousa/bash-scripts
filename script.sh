15653be5cab885f2ba288047848e066fbcb655d2
Osama Mousa <83459975+osama-mousa@users.noreply.github.com>
2024-08-03 13:10:23 +0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 
-# تعريف متغير
 name="ChatGPT"
-
-# عرض رسالة على الشاشة
 echo "Hello, $name!"
-
-# جملة شرطية
 if [ $name == "ChatGPT" ]; then
     echo "Welcome to Bash scripting!"
 else
