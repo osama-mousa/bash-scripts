f415586b148897614b10eda8cceca12f049d77c4
Osama Mousa <83459975+osama-mousa@users.noreply.github.com>
2025-04-30 02:51:24 +0300

    Add files via upload

---
+++ googleChrome-setup.sh
@@ -0,0 +1,7 @@
+sudo apt update -y
+sudo apt install wget -y
+wget  https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
+sudo dpkg -i google-chrome-stable_current_amd64.deb
+google-chorme
+cat /etc/apt/sources.list.d/google-chrome.list
+sudo apt upgrade google-chrome-stable -y
